120ebdbc7be6e25a0c032880c34d18728ae5a00a
rb225 <rb225@njit.edu>
2021-12-13 02:18:26 -0500

    updated code

---
+++ calc/calculations/division.py
@@ -0,0 +1,13 @@
+""" Class for division"""
+from calc.calculations.calculation import Calculation
+
+
+class Division(Calculation):
+    """Division calculation object"""
+
+    def get_result(self):
+        """Performing division function for the values inheriting from the parent class"""
+        result = self.values[0]
+        for value in self.values[1:]:
+            result = result / value
+        return result
--- calc/calculations/subtraction.py
+++ calc/calculations/subtraction.py
@@ -7,8 +7,8 @@ class Subtraction(Calculation):
     """subtraction calculation object"""
     def get_result(self):
         """get the subtraction results"""
-        difference_of_values = 0.0
-        for value in self.values:
+        difference_of_values = self.values[0]
+        for value in self.values[1:]:
             difference_of_values =   difference_of_values - value
             pprint.pprint(value)
         return difference_of_values
